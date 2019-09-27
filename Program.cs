d3ddef937a75fdd6d6ab1cb21d089211808246ec
lisa4473 <54340604+lisa4473@users.noreply.github.com>
2019-09-27 11:27:47 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Methodshw
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int result = Add(4, 8);
+            if (result > 4)
+                Console.WriteLine("Result is larger than 4");
+            else
+                Console.WriteLine("result is smaller than or equal to 4");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+        public static void add(int num1, int num2)
+        {
+
+            Console.WriteLine(num1 * num2);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+
+
+        }
+        public static void main()
+        {
+            int i = 0;
+            ProgramMethod(i);
+            Console.WriteLine(i);
+        }
+        public static void ProgramMethod(int l)
+        {
+            l = 100;
+        }
+
+        public static void main(string[] args) {
+            int[] love = { 20, 40, 60, 80 };
+        }
+        public static void display(int[] input) {
+
+          for(int love=0; love < input.Length; love++) { 
+                
+         }
+            // int main
+            {
+                
+                var prog = new Program();
+                program.Method 1();
+                int returned =prog.method2();
+            }
+                 
+            {
+
+                Console.WriteLine("Method success");
+            }
+
+            public int Mymethod1 ();
+            Console.WriteLine("method 2 success");
+            return 7;
+            {
+
+
+            } 
+
+
+
+
+
+
+
+
